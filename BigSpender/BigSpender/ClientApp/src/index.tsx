f9bc83bc29a27f7008da2c5fbadd7365781177b6
Artem Astashenkov <astashenkovaa@gmail.com>
2019-11-16 22:32:10 +0200

    Added Roboto font for Windows compatibility

--- BigSpender/BigSpender/ClientApp/src/index.tsx
+++ BigSpender/BigSpender/ClientApp/src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Grommet } from 'grommet';
+import 'typeface-roboto';
 
 const myTheme = {
   global: {
